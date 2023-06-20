b8e1b523c7ad5b43762d80b0095b78eecd30ccb5
Anjum <88701810+Anjum7866@users.noreply.github.com>
2023-06-20 12:22:35 -0700

    dom removed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {ThemeProvider, createGlobalStyle} from 'styled-components';
+// import {ThemeProvider, createGlobalStyle} from 'styled-components';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -11,23 +11,24 @@ import About from './contents/About';
 import Portfolio from './contents/Portfolio';
 import Contact from './contents/Contact';
 
-const GlobalStyle = createGlobalStyle`
-  body{
-  background: linear-gradient(${props => props.theme.mode === 'dark' ? '#868f96, #596164' : '(#ff758c,#764ba2'});
-  }
+// const GlobalStyle = createGlobalStyle`
+//   body{
+//   background: linear-gradient(${props => props.theme.mode === 'dark' ? '#868f96, #596164' : '(#ff758c,#764ba2'});
+//   }
 
-`
+// `
 
 function App() {
   const [theme, setTheme] = useState({ mode: 'light'});
   return (
-    <ThemeProvider theme={theme}>
+    // <ThemeProvider theme={theme}>
       <>
-    <GlobalStyle />
+    {/* <GlobalStyle /> */}
     <Router>
     <div className="App">
     <Navbar />
-    <Route exact path="/">
+    <Home />
+    {/* <Route exact path="/">
     <Home />
     </Route>
     <Route path="/about">
@@ -38,14 +39,14 @@ function App() {
     </Route>
     <Route path="/contact">
     <Contact />
-    </Route>
+    </Route> */}
     <div class="back1 glass" onClick={e => setTheme(
       theme.mode === 'dark' ? {mode: 'light'} : {mode: 'dark'}
     )}><i class={theme.mode === 'dark' ? "fa fa-sun i1" : "fa fa-moon i1"}></i></div>
     </div>
     </Router>
     </>
-    </ThemeProvider>
+    // </ThemeProvider>
     );
   }
   
